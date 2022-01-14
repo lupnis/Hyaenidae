752dd3c9e395388918e3400131a8655ef2737f8e
Lupnis J H <65355238+lupnis@users.noreply.github.com>
2022-01-14 21:33:35 +0800

    update search page

--- Hyaenidae/Hyaenidae/Startup.cs
+++ Hyaenidae/Hyaenidae/Startup.cs
@@ -1,4 +1,3 @@
-/*fin*/
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
---
+++ Hyaenidae/Hyaenidae/wwwroot/js/cuon_listbox.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    var list_box_items = document.getElementsByClassName('gallery_box_container');
+    for (var i = 0; i < list_box_items.length; i++) {
+        list_box_items[i].getElementsByTagName('h3')[0].onclick = list_box_items[i].getElementsByTagName('p')[0].onclick = list_box_items[i].getElementsByTagName('input')[0].onclick = function (e) {
+            window.location = this.parentElement.getAttribute('href');
+        }
+    }
+})();
