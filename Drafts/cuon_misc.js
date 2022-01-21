ca8d62335b2318dde36201713fc3fcaabe2d375c
Lupnis <Lupnis@outlook.com>
2022-01-21 22:15:37 +0800

    界面完成，清理草稿，仅保留未做的用户登录部分草稿，目前无需使用用户部分

--- Drafts/cuon_misc.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    history_back_btns = document.getElementsByClassName('history_back_btn');
-    for (var i = 0; i < history_back_btns.length; i++) {
-        history_back_btns[i].addEventListener('click', function() {
-            history.back();
-        });
-    }
-})();
---
+++ Hyaenidae/Hyaenidae/Controllers/DatasetsApiController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hyaenidae.Controllers
+{
+    public class DatasetsApiController : ControllerBase
+    {
+        // GET: DatasetsApiController
+        public string Index()
+        {
+            return "service temporarily unavailable";
+        }
+
+        // GET: DatasetsApiController/Details/5
+        public string Details(int id)
+        {
+            return "service temporarily unavailable";
+        }
+
+        // GET: DatasetsApiController/Create
+        public string Create()
+        {
+            return "service temporarily unavailable";
+        }
+
+        // POST: DatasetsApiController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public string Create(IFormCollection collection)
+        {
+            return "service temporarily unavailable";
+        }
+
+        // GET: DatasetsApiController/Edit/5
+        public string Edit(int id)
+        {
+            return "service temporarily unavailable";
+        }
+
+        // POST: DatasetsApiController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public string Edit(int id, IFormCollection collection)
+        {
+            return "service temporarily unavailable";
+        }
+
+        // GET: DatasetsApiController/Delete/5
+        public string Delete(int id)
+        {
+            return "service temporarily unavailable";
+        }
+
+        // POST: DatasetsApiController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public string Delete(int id, IFormCollection collection)
+        {
+            return "service temporarily unavailable";
+        }
+    }
+}
