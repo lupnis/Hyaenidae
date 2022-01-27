8189feec2730a571bbddafc97bb51be99f89a01b
Lupnis <Lupnis@outlook.com>
2022-01-27 22:58:12 +0800

    daily upd

--- Hyaenidae/Hyaenidae/Misc/DatasourceOperation.cs
+++ Hyaenidae/Hyaenidae/Misc/DatasourceOperation.cs
@@ -6,5 +6,6 @@ namespace Hyaenidae.Misc
         //TODO: data exchange with MySQL and Redis
         //hyaenidae<=>redis<=>sql<=>hyaenidae
         //uwu
+        //HOW TO USE REDIS IN EXTENSIONS.CACHING.STACKEXCHANGEREDIS qwq
     }
 }
