f81b8694d39692be0cffd0b173de2e24839b59ed
Lupnis <Lupnis@outlook.com>
2022-01-30 17:12:17 +0800

    update log

--- Hyaenidae/Storenidae/Log/DiskLogController.cs
+++ Hyaenidae/Storenidae/Log/DiskLogController.cs
@@ -9,19 +9,32 @@ namespace Storenidae.Log
     internal class DiskLogController
     {
         private string logPath;
-
-
-        public void AppendLog(string _content)
+        private bool autoFlush = false;
+        private uint flushSize = 128;
+        protected List<String> logs;
+        public DiskLogController()
         {
 
+        }
+        public void AppendLog(string _content)
+        {
+            logs.Add(_content);
+            if (autoFlush && logs.Count == flushSize)
+            {
+                FlushLog();
+            }
         }
         public void FlushLog()
         {
-
+            WriteLog();
+            logs.Clear();
         }
-        public void DropLog()
+        public void DropLog()// only drop cached data hasnt been stored on disk
         {
 
+        }
+        private void WriteLog()
+        {
         }
     }
 }
