5f725b8b1bd23e79f382ffb484a81ab931ab48ce
Lupnis <Lupnis@outlook.com>
2022-01-31 23:35:55 +0800

    update database part

--- Hyaenidae/Storenidae/Data/GlobalData.cs
+++ Hyaenidae/Storenidae/Data/GlobalData.cs
@@ -8,7 +8,7 @@ namespace Storenidae.Data
 {
     internal class GlobalData
     {
-        protected const string MySQLConnectionString = "";
+        protected const string MySQLConnectionString = "server=127.0.0.1;port=3306;user=root;password=root; database=minecraftdb;";
         protected const string RedisConnectionString = "127.0.0.1:6379";
     }
 }
---
+++ Hyaenidae/Storenidae/Locale/MySQLBatchFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storenidae.Locale
+{
+    internal class MySQLBatchFile
+    {
+        public const string databaseEnv = @"
+create database if not exists db_hyaenidae_sto DEFAULT CHARACTER SET utf8;
+create xwxwxwxwxwxwxwxwxwxwxwxwx
+";
+    }
+}
