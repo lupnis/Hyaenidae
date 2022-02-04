dc03ad70c364cf6185dbf8ac37ed6a1950d8d1b7
Lupnis <Lupnis@outlook.com>
2022-02-04 22:27:21 +0800

    update

--- Hyaenidae/Storenidae/Data/GlobalData.cs
+++ Hyaenidae/Storenidae/Data/GlobalData.cs
@@ -8,7 +8,7 @@ namespace Storenidae.Data
 {
     internal class GlobalData
     {
-        protected const string MySQLConnectionString = "server=127.0.0.1;port=3306;user=root;password=root; database=minecraftdb;";
+        protected const string MySQLConnectionString = "server=127.0.0.1;port=3306;user=db_hyaenidae;password=db_hyaenidaepp; database=db_hyaenidae_sto;";
         protected const string RedisConnectionString = "127.0.0.1:6379";
     }
 }
